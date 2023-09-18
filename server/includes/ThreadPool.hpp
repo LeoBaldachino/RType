48c6ab424f84fdc54adc3913d0a814545690fa04
cote2porc <robin.denni@epitech.eu>
2023-09-18 13:32:23 +0200

    feat(server/includes) base for the architecture of the server 2, threadpool and worker

---
+++ server/includes/ThreadPool.hpp
@@ -0,0 +1,17 @@
+/*
+** EPITECH PROJECT, 2023
+** B-CPP-500-MLH-5-1-rtype-robin.denni
+** File description:
+** ThreadPool
+*/
+
+#pragma once
+
+class ThreadPool {
+    public:
+        ThreadPool();
+        ~ThreadPool();
+
+    protected:
+    private:
+};
