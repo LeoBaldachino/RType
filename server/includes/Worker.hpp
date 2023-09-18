97783be9076d1cc783d7edfebc359cd2c5d9400b
cote2porc <robin.denni@epitech.eu>
2023-09-18 15:18:13 +0200

    feat(Sockets) add the base for the sockets

---
+++ server/includes/Worker.hpp
@@ -0,0 +1,17 @@
+/*
+** EPITECH PROJECT, 2023
+** B-CPP-500-MLH-5-1-rtype-robin.denni
+** File description:
+** Worker
+*/
+
+#pragma once
+
+class Worker {
+    public:
+        Worker();
+        ~Worker();
+
+    protected:
+    private:
+};
