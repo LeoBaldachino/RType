425bf96de7d4d17836fe0f952eafdb49e8c0f5cd
TheotimeS <theotime.schmeltz@epitech.eu>
2023-09-25 17:47:20 +0200

    {|} Modified Systems' files' name

---
+++ Systems/BydosSystem.cpp
@@ -0,0 +1,8 @@
+/*
+** EPITECH PROJECT, 2023
+** B-CPP-500-MLH-5-1-rtype-robin.denni
+** File description:
+** BydosSystem.cpp
+*/
+
+#include "BydosSystem.hpp"
---
+++ Systems/TourreSystem.hpp
@@ -0,0 +1,9 @@
+/*
+** EPITECH PROJECT, 2023
+** B-CPP-500-MLH-5-1-rtype-robin.denni
+** File description:
+** TourreSystem.hpp
+*/
+
+#pragma once
+
