0d8d9b048794877157070fe456605ae0bac60a38
Liebenguth Alessio <alessio.liebenguth@epitech.eu>
2023-09-25 09:23:13 +0200

    {+} add Player entity class in hpp + cpp

--- Entity/Player.cpp
+++ Entity/Player.cpp
@@ -6,3 +6,69 @@
 */
 
 #include "Player.hpp"
+
+Player::Player(Position position, int velocity, Vector2d moveDirection)
+{
+    this->_position = position;
+    this->_state = State(100);
+    this->_movement = Moveable(Vector2d(this->_position.getX(), this->_position.getY()), moveDirection, velocity);
+}
+
+Shoot Player::shoot() const
+{
+    Vector2d shootDirection = {1, 0};
+    Vector2d playerPos = {this->_position.getX(), this->_position.getY()};
+
+
+    return Shoot(shootDirection, playerPos, this->_shootDmg, this->_shootVelocity, this->_shootGravity, true);
+}
+/* Player position methods */
+
+void Player::move(Vector2d direction)
+{
+
+}
+
+void Player::setPosition(Position position)
+{
+    this->_position = position;
+}
+
+Position Player::getPosition() const
+{
+    return this->_position;
+}
+
+/* Player Hitbox & State methods */
+
+void Player::setHitbox(Hitbox hitbox)
+{
+    // this->_hitbox = hitbox;
+}
+
+Hitbox Player::getHitbox() const
+{
+    // return this->_hitbox;
+}
+
+void Player::setState(State state)
+{
+    this->_state = state;
+}
+
+State Player::getState() const
+{
+    return this->_state;
+}
+
+/* Player movements methods */
+
+void Player::setMoveable(Moveable moveable)
+{
+    this->_movement = moveable;
+}
+
+Moveable Player::getMoveable() const
+{
+    return this->_movement;
+}
--- Entity/Player.hpp
+++ Entity/Player.hpp
@@ -12,3 +12,36 @@
 #include "../Components/Controlable.hpp"
 #include "../Components/Hitbox.hpp"
 #include "../Components/Moveable.hpp"
+
+class Player {
+    public:
+        Player(Position position, int velocity, Vector2d moveDirection);
+        ~Player() {};
+
+        Shoot shoot() const;
+
+        void handleInput(sf::Event event, std::unique_ptr<sf::RenderWindow> window);
+
+        void move(Vector2d direction);
+
+        void setPosition(Position position);
+        Position getPosition() const;
+
+        void setHitbox(Hitbox hitbox);
+        Hitbox getHitbox() const;
+
+        void setState(State state);
+        State getState() const;
+
+        void setMoveable(Moveable moveable);
+        Moveable getMoveable() const;
+
+    private:
+        State _state;
+        Position _position;
+        Moveable _movement;
+        // Hitbox _hitbox;
+        int _shootDmg = 10;
+        int _shootVelocity = 10;
+        int _shootGravity = 0;
+};
