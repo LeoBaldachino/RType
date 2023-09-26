5e37e1820cf0570de2aca0e1b46979e75f8ebe3a
Liebenguth Alessio <alessio.liebenguth@epitech.eu>
2023-09-26 14:16:13 +0200

    {+} Subsystem State class filled with constructor + setState method

---
+++ Systems/Subsystems/StateSystem.cpp
@@ -0,0 +1,23 @@
+/*
+** EPITECH PROJECT, 2023
+** B-CPP-500-MLH-5-1-rtype-robin.denni
+** File description:
+** StateSystem.cpp
+*/
+
+#include "StateSystem.hpp"
+
+StateSystem::StateSystem(const State &state)
+{
+    this->_state = state;
+}
+
+StateSystem::~StateSystem()
+{
+
+}
+
+void StateSystem::setState(const State &state)
+{
+    this->_state = state;
+}
---
+++ Systems/Subsystems/StateSystem.hpp
@@ -0,0 +1,21 @@
+/*
+** EPITECH PROJECT, 2023
+** B-CPP-500-MLH-5-1-rtype-robin.denni
+** File description:
+** StateSystem.hpp
+*/
+
+#pragma once
+
+#include "../../Components/State.hpp"
+
+class StateSystem {
+    public:
+        StateSystem(const State &state);
+        ~StateSystem();
+
+        void setState(const State &state);
+
+    private:
+        State _state;
+};
