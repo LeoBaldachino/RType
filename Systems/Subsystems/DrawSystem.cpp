081cab654a6f7ae1e3fd24045ea298c233b84d2e
TheotimeS <theotime.schmeltz@epitech.eu>
2023-09-26 14:04:37 +0200

    {|} Moved DrawSystem.hpp code to DrawSystem.cpp

---
+++ Systems/Subsystems/DrawSystem.cpp
@@ -0,0 +1,31 @@
+/*
+** EPITECH PROJECT, 2023
+** B-CPP-500-MLH-5-1-rtype-robin.denni
+** File description:
+** DrawSystem.cpp
+*/
+
+#include "DrawSystem.hpp"
+
+DrawSystem::DrawSystem(const Drawable &drawable, const Position &position)
+{
+    this->_drawable = drawable;
+    this->_position = position;
+}
+
+void DrawSystem::setDrawable(const Drawable &drawable)
+{
+    this->_drawable = drawable;
+}
+
+void DrawSystem::setPosition(const Position &position)
+{
+    this->_position = position;
+}
+
+void DrawSystem::draw(std::unique_ptr<sf::RenderWindow> window)
+{
+    sf::Sprite tmpSprite = this->_drawable.getSprite();
+    tmpSprite.setPosition(sf::Vector2f(this->_position.getX(), this->_position.getY()));
+    window->draw(tmpSprite);
+}
--- Systems/Subsystems/DrawSystem.hpp
+++ Systems/Subsystems/DrawSystem.hpp
@@ -10,15 +10,11 @@
 
 class DrawSystem {
     public:
-        DrawSystem(const Drawable &drawable, const Position &position) { this->_drawable = drawable; this->_position = position; }
+        DrawSystem(const Drawable &drawable, const Position &position);
         ~DrawSystem() {};
-        void setDrawable(const Drawable &drawable) { this->_drawable = drawable; }
-        void setPosition(const Position &position) { this->_position = position; }
-        void draw(std::unique_ptr<sf::RenderWindow> window) {
-            sf::Sprite tmpSprite = this->_drawable.getSprite();
-            tmpSprite.setPosition(sf::Vector2f(this->_position.getX(), this->_position.getY()));
-            window->draw(tmpSprite);
-        }
+        void setDrawable(const Drawable &drawable);
+        void setPosition(const Position &position);
+        void draw(std::unique_ptr<sf::RenderWindow> window);
     private:
         Drawable _drawable;
         Position _position;
