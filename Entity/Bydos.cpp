40ab5ebc33bc9c123fcd084c372b14ddfb20c7a4
Liebenguth Alessio <alessio.liebenguth@epitech.eu>
2023-09-27 21:51:51 +0200

    {+} filled bydos class & cpp with setters + getters + accept method

--- Entity/Bydos.cpp
+++ Entity/Bydos.cpp
@@ -7,15 +7,91 @@
 
 #include "Bydos.hpp"
 
-Bydos::Bydos(Position position, int velocity, Vector2d moveDirection)
+Bydos::Bydos(Position position, int velocity, Vector2d moveDirection) : _drawable("Assets/player.png", 2)
 {
     this->_position = position;
     this->_state = State(100);
     this->_movement = Moveable(Vector2d(this->_position.getX(), this->_position.getY()), moveDirection, velocity);
 }
 
-Shoot Bydos::shoot(const Position &playerPos) const
-{   
-    AIShoot aiShoot(playerPos, this->_position);
-    return aiShoot.shootLogic();
+// Shoot Bydos::shoot(const Position &playerPos) const
+// {   
+//     AIShoot aiShoot(playerPos, this->_position);
+//     return aiShoot.shootLogic();
+// }
+
+void Bydos::accept(IVisitor &v, std::unique_ptr<sf::RenderWindow> &window)
+{
+    v.visitBydos(*this, window);
 }
+
+void Bydos::setPosition(Position position)
+{
+    this->_position = position;
+}
+
+Position Bydos::getPosition() const
+{
+    return this->_position;
+}
+
+void Bydos::setState(State state)
+{
+    this->_state = state;
+}
+
+State Bydos::getState() const
+{
+    return this->_state;
+}
+
+void Bydos::setDrawable(Drawable drawable)
+{
+    this->_drawable = drawable;
+}
+
+Drawable Bydos::getDrawable() const
+{
+    return this->_drawable;
+}
+
+void Bydos::setVelocity(int velocity)
+{
+    this->_velocity = velocity;
+}
+
+int Bydos::getVelocity(void) const
+{
+    return this->_velocity;
+}
+
+void Bydos::setShootDmg(int shootDmg)
+{
+    this->_shootDmg = shootDmg;
+}
+
+int Bydos::getShootDmg(void) const
+{
+    return this->_shootDmg;
+}
+
+void Bydos::setShootVelocity(int shootVelocity)
+{
+    this->_shootVelocity = shootVelocity;
+}
+
+int Bydos::getShootVelocity(void) const
+{
+    return this->_shootVelocity;
+}
+
+void Bydos::setShootGravity(int shootGravity)
+{
+    this->_shootGravity = shootGravity;
+}
+
+int Bydos::getShootGravity(void) const
+{
+    return this->_shootGravity;
+}
+
--- Entity/Bydos.hpp
+++ Entity/Bydos.hpp
@@ -6,18 +6,52 @@
 */
 
 #pragma once
+#include "IEntity.hpp"
 #include "../Components/Moveable.hpp"
 #include "../Components/Position.hpp"
 #include "../Components/State.hpp"
 #include "../Systems/Subsystems/AIShoot.hpp"
 #include "../Components/Hitbox.hpp"
+#include "../Components/Drawable.hpp"
 
-class Bydos {
+class Bydos : public IEntity {
     public:
+        Bydos() {};
         Bydos(Position position, int velocity, Vector2d moveDirection);
-        Shoot shoot(const Position &playerPos) const;
+        ~Bydos() {};
+        void accept(IVisitor &v, std::unique_ptr<sf::RenderWindow> &window);
+
+        // Shoot shoot(const Position &playerPos) const;
+
+        void setPosition(Position position);
+        Position getPosition() const;
+
+        void setState(State state);
+        State getState() const;
+
+        void setDrawable(Drawable drawable);
+        Drawable getDrawable() const;
+
+        void setVelocity(int velocity);
+        int getVelocity(void) const;
+
+        void setShootDmg(int shootDmg);
+        int getShootDmg(void) const;
+
+        void setShootVelocity(int shootVelocity);
+        int getShootVelocity(void) const;
+
+        void setShootGravity(int shootGravity);
+        int getShootGravity(void) const;
+
     private:
         State _state;
         Position _position;
         Moveable _movement;
+        Drawable _drawable;
+        // AIShoot _aishoot;
+        int _velocity;
+        int _shootDmg = 10;
+        int _shootVelocity = 5;
+        int _shootGravity = 0;
 };
