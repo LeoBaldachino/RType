28c704494df67df22d5e4b7c001b0251e37374be
TheotimeS <theotime.schmeltz@epitech.eu>
2023-09-28 11:19:22 +0200

    {|} Added Bydos instances in main

--- main.cpp
+++ main.cpp
@@ -11,11 +11,15 @@
 int main(void)
 {
     std::unique_ptr<sf::RenderWindow> _window = std::make_unique<sf::RenderWindow>(sf::VideoMode::getDesktopMode(), "R-Type");
-    Player p(Position(0, 0, 1080, 1920));
     SystemVisitor v;
+    std::vector<std::unique_ptr<IEntity>> entities;
+    entities.push_back(std::make_unique<Player>(Position(0, 0, 1080, 1920)));
+    entities.push_back(std::make_unique<Bydos>(Position(1900, 100, 1080, 1920), 10, Vector2d(-1, 0)));
+    entities.push_back(std::make_unique<Bydos>(Position(1900, 300, 1080, 1920), 10, Vector2d(-1, 0)));
     while (_window->isOpen()) {
         _window->clear();
-        p.accept(v, p, _window);
+        for (int i = 0; i != entities.size(); ++i)
+            entities[i]->accept(v, _window);
         _window->display();
     }
 }
