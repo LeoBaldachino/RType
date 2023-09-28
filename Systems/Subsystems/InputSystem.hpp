fdb604e39ecd550a7cffc3690cf0c83defbcc276
TheotimeS <theotime.schmeltz@epitech.eu>
2023-09-28 11:53:45 +0200

    {|} Fixed Player's inputs

--- Systems/Subsystems/InputSystem.hpp
+++ Systems/Subsystems/InputSystem.hpp
@@ -10,12 +10,18 @@
 #include <SFML/Window/Keyboard.hpp>
 #include <SFML/Window/Event.hpp>
 #include <chrono>
+#include <unordered_map>
 #include <SFML/Graphics.hpp>
 #include "../../Entity/Player.hpp"
 
 class InputSystem {
     public:
-        InputSystem() : _inputs() {};
+        InputSystem() : _inputs() { this->_keysDown = {
+            {Inputs::Events::Up, false},
+            {Inputs::Events::Down, false},
+            {Inputs::Events::Left, false},
+            {Inputs::Events::Right, false}};
+        };
         ~InputSystem() = default;
         void handleInput(sf::Event event, std::unique_ptr<sf::RenderWindow> &window);
         void updatePlayer(Player &player);
@@ -23,4 +29,5 @@ class InputSystem {
         Inputs _inputs;
         std::chrono::time_point<std::chrono::_V2::steady_clock, std::chrono::_V2::steady_clock::duration> shotTime;
         bool shooting = false;
+        std::unordered_map<Inputs::Events, bool> _keysDown;
 };
