7254ae26ef80de73008233687f82411922784841
Liebenguth Alessio <alessio.liebenguth@epitech.eu>
2023-10-17 13:30:03 +0200

    {+} add tourre entity handling in visitor + update tourreSystem class and methods

--- Systems/TourreSystem.cpp
+++ Systems/TourreSystem.cpp
@@ -7,14 +7,31 @@
 
 #include "TourreSystem.hpp"
 
+TourreSystem::TourreSystem()
+{
+}
+
 void TourreSystem::updatePos(Tourre &t)
 {
+    if (!t.readyToMove())
+        return;
     this->_movementSystem.updatePosition(t);
     t.setHasMoved(true);
 }
 
-void TourreSystem::createShots(Tourre &t, const Player &player, Core &core)
+// void TourreSystem::createShots(Tourre &t, const Player &player, Core &core)
+// {
+//     Shoot tmpShoot(t.shoot(player.getPosition()));
+//     core.addEntity(std::make_shared<ShotEntity>(tmpShoot, "../Assets/enemyShot.png", false), core.getAvailabeIndex());
+// }
+
+void TourreSystem::checkCollision(Tourre &t, IEntity &entity, Core &core, bool isTouching)
 {
-    Shoot tmpShoot(t.shoot(player.getPosition()));
-    core.addEntity(std::make_shared<ShotEntity>(tmpShoot, "../Assets/enemyShot.png", false), core.getAvailabeIndex());
+    if (this->_hitBoxSystem.entityIntersect(t, entity)) {
+        if (t.getLifes() >= 1)
+            if (t.removeOneLife())
+                std::cout << "One life removed from tourre" << std::endl;
+        if (!isTouching)
+            core.removeEntityLater(entity);
+    }
 }
