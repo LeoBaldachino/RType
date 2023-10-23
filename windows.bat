8fde2e7a92debb63869d32a7a1c7057300a50f0d
unknown <leo.baldachino@epitech.eu>
2023-10-23 14:45:48 +0200

    [+]Started working on windows compilation [+] Bat script that installs boost (boost is not compatible with CMakes as it is) [!] Commented SFML part of cmake (uncomment it at will)

---
+++ windows.bat
@@ -0,0 +1,14 @@
+@echo off
+setlocal
+
+cls
+
+if exist boost (
+   echo "Boost library found."
+) else (
+    echo "Boost library not found. Installing submodules..."
+    git submodule update --init --recursive
+)
+
+cd boost
+b2.exe headers
